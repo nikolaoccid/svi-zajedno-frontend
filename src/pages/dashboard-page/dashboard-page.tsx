535132903982e83c599e80fb82110282ca1cee6a
Nikola Occidentale <nikolaoccid@gmail.com>
2023-08-16 19:06:26 +0200

    Add dasboard menu

--- src/pages/dashboard-page/dashboard-page.tsx
+++ src/pages/dashboard-page/dashboard-page.tsx
@@ -1,7 +1,9 @@
 import styled from '@emotion/styled';
 import { useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { BounceLoader } from 'react-spinners';
 
+import projectAssociateImage from '/project-associate.png';
 import projectUserImage from '/project-users.png';
 
 import { toastInfo } from '../../utils/toast.ts';
@@ -11,13 +13,15 @@ import { useSelectedSchoolYear } from './hooks/use-fetch-school-year.ts';
 const Menu = styled.div`
   display: flex;
   flex-direction: row;
-  gap: 50px;
+  gap: 70px;
 `;
 const MenuItem = styled.div`
   display: flex;
   flex-direction: row;
-  width: 33%;
   gap: 20px;
+  padding: 20px;
+  border-radius: 4px;
+  border: 2px solid lightgray;
 `;
 const MenuContent = styled.div`
   display: flex;
@@ -25,7 +29,14 @@ const MenuContent = styled.div`
   flex-direction: column;
 `;
 const MenuImage = styled.img`
-  width: 120px;
+  width: 144px;
+`;
+export const DashboardNavLink = styled(Link)`
+  color: black;
+  text-decoration: none;
+  &:hover {
+    text-decoration: underline;
+  }
 `;
 export function DashboardPage() {
   const { data: schoolYear, isLoading: isLoadingSchoolYear, error } = useSelectedSchoolYear();
@@ -50,9 +61,9 @@ export function DashboardPage() {
       <CenterContent>
         <AlignRight>
           {schoolYear && (
-            <NavLink to="/school-year">
+            <DashboardNavLink to="/school-year">
               School year: {schoolYear[0].startYear}/{schoolYear[0].endYear}{' '}
-            </NavLink>
+            </DashboardNavLink>
           )}
         </AlignRight>
         <h1>Dashboard page</h1>
@@ -60,15 +71,17 @@ export function DashboardPage() {
           <MenuItem>
             <MenuImage src={projectUserImage} />
             <MenuContent>
-              <NavLink to={`${schoolYear}/users`}>Svi korisnici</NavLink>
-              <NavLink to={`${schoolYear}/user`}>Kreiraj novog korisnika</NavLink>
+              <h2>KORISNICI</h2>
+              <DashboardNavLink to={`${schoolYear}/users`}>Svi korisnici</DashboardNavLink>
+              <DashboardNavLink to={`${schoolYear}/user`}>Kreiraj novog korisnika</DashboardNavLink>
             </MenuContent>
           </MenuItem>
           <MenuItem>
-            <MenuImage src={projectUserImage} />
+            <MenuImage src={projectAssociateImage} />
             <MenuContent>
-              <NavLink to={`${schoolYear}/users`}>Svi korisnici</NavLink>
-              <NavLink to={`${schoolYear}/user`}>Kreiraj novog korisnika</NavLink>
+              <h2>SURADNICI</h2>
+              <NavLink to={`${schoolYear}/users`}>Svi suradnici</NavLink>
+              <NavLink to={`${schoolYear}/user`}>Kreiraj novog suradnika</NavLink>
             </MenuContent>
           </MenuItem>
         </Menu>
