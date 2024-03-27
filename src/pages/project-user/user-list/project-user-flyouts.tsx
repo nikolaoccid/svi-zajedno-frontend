348ead91d65fa7e0580646a67444b205c778e9c8
Nikola Occidentale <nikolaoccid@gmail.com>
2024-03-27 02:12:35 +0100

    Move project user flyouts

---
+++ src/pages/project-user/user-list/project-user-flyouts.tsx
@@ -0,0 +1,83 @@
+import { useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { useLocation, useNavigate, useParams } from 'react-router-dom';
+
+import { FlyoutComponent } from '../../../components/flyout/flyout-component.tsx';
+import ManageProjectUserView from '../create-project-user/manage-project-user-view.tsx';
+import { EnrollStudentOnSchoolYear } from '../enroll-student-on-school-year/enroll-student-on-school-year.tsx';
+import { ManageStudentOnActivityContainer } from '../manage-student-on-activity/manage-student-on-activity-container.tsx';
+import { EditUserOnActivity } from '../user-view/edit-user-on-activity.tsx';
+import UserView from '../user-view/user-view.tsx';
+interface FlyoutData {
+  onHide: () => void;
+  RenderComponent: any;
+  flyoutTitle: string;
+}
+export function ProjectUserFlyouts() {
+  const { t } = useTranslation();
+  const { startYear, userId, activityId } = useParams();
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+
+  const flyoutComponents = {
+    manageProjectUser: {
+      onHide: () => navigate(`/${startYear}/users`),
+      RenderComponent: ManageProjectUserView,
+      flyoutTitle: t('User profile'),
+    },
+    userView: {
+      onHide: () => navigate(`/${startYear}/users`),
+      RenderComponent: UserView,
+      flyoutTitle: t('User profile'),
+    },
+    manageStudentOnActivity: {
+      onHide: () => navigate(`/${startYear}/users/${userId}`),
+      RenderComponent: ManageStudentOnActivityContainer,
+      flyoutTitle: t('Enroll user on activity'),
+    },
+    enrollStudentOnSchoolYear: {
+      onHide: () => navigate(`/${startYear}/users/${userId}`),
+      RenderComponent: EnrollStudentOnSchoolYear,
+      flyoutTitle: t('Enroll user on the academic year'),
+    },
+    editUser: {
+      onHide: () => navigate(`/${startYear}/users/${userId}`),
+      RenderComponent: EditUserOnActivity,
+      flyoutTitle: t('Edit user'),
+    },
+  };
+
+  const [flyoutData, setFlyoutData] = useState<FlyoutData | null>(null);
+
+  useEffect(() => {
+    const pathWithoutParams = pathname.split('?')[0];
+    const paths = {
+      [`/${startYear}/users/new`]: 'manageProjectUser',
+      [`/${startYear}/users/${userId}/edit`]: 'manageProjectUser',
+      [`/${startYear}/users/${userId}/activities/new`]: 'manageStudentOnActivity',
+      [`/${startYear}/users/${userId}/enroll`]: 'enrollStudentOnSchoolYear',
+      [`/${startYear}/users/${userId}`]: 'userView',
+      [`/${startYear}/users/${userId}/activities/${activityId}/edit`]: 'manageStudentOnActivity',
+    };
+
+    const flyoutName = paths[pathWithoutParams];
+    if (flyoutName) {
+      setFlyoutData(flyoutComponents[flyoutName]);
+    } else {
+      setFlyoutData(null);
+    }
+  }, [pathname, startYear, userId, activityId, flyoutComponents]);
+
+  return (
+    <>
+      {flyoutData && (
+        <FlyoutComponent
+          shouldShow={true}
+          flyoutTitle={flyoutData.flyoutTitle}
+          onHide={flyoutData.onHide}
+          RenderComponent={flyoutData.RenderComponent}
+        />
+      )}
+    </>
+  );
+}
--- src/pages/project-user/user-list/user-list-container.tsx
+++ src/pages/project-user/user-list/user-list-container.tsx
@@ -1,17 +1,11 @@
 import styled from '@emotion/styled';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { useAsync } from 'react-async-hook';
-import { useTranslation } from 'react-i18next';
-import { useLocation, useNavigate, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 
-import { FlyoutComponent } from '../../../components/flyout/flyout-component.tsx';
 import { DashboardContainer } from '../../dashboard/dashboard.tsx';
 import { useSchoolYear } from '../../dashboard-page/hooks/use-fetch-school-year.ts';
-import ManageProjectUserView from '../create-project-user/manage-project-user-view.tsx';
-import { EnrollStudentOnSchoolYear } from '../enroll-student-on-school-year/enroll-student-on-school-year.tsx';
-import { ManageStudentOnActivityContainer } from '../manage-student-on-activity/manage-student-on-activity-container.tsx';
-import { EditUserOnActivity } from '../user-view/edit-user-on-activity.tsx';
-import UserView from '../user-view/user-view.tsx';
+import { ProjectUserFlyouts } from './project-user-flyouts.tsx';
 import { useGetProjectUserByPageAndQuery, useGetProjectUsersBySchoolYear } from './useGetProjectUserByPageAndQuery.ts';
 import { UserList } from './user-list.tsx';
 
@@ -27,78 +21,9 @@ export const HeaderSubtext = styled.span`
 `;
 
 export function UserListContainer() {
-  const { t } = useTranslation();
-  const { startYear, userId, activityId } = useParams();
+  const { startYear } = useParams();
   const { data: schoolYear } = useSchoolYear(startYear ? parseInt(startYear ?? '0') : 0);
-  const navigate = useNavigate();
-  const { pathname } = useLocation();
 
-  const [showAddEditFlyout, setShowAddEditFlyout] = useState(false);
-  const [showUserFlyout, setShowUserFlyout] = useState(false);
-  const [showAddActivityFlyout, setShowAddActivityFlyout] = useState(false);
-  const [flyoutTitle, setFlyoutTitle] = useState('Uredi korisnika');
-  const [showEnrollOnSchoolYear, setShowEnrollOnSchoolYear] = useState(false);
-  const [showUnenrollFlyout, setShowUnenrollFlyout] = useState(false);
-
-  useEffect(() => {
-    const pathWithoutParams = pathname.split('?')[0];
-    const isNewUserPath = pathWithoutParams === `/${startYear}/users/new`;
-    const isEditUserPath = pathWithoutParams === `/${startYear}/users/${userId}/edit`;
-    const isUserActivitiesNewPath = pathWithoutParams === `/${startYear}/users/${userId}/activities/new`;
-    const isUserPath = pathWithoutParams === `/${startYear}/users/${userId}`;
-    const enrollUserOnSchoolYear = pathWithoutParams === `/${startYear}/users/${userId}/enroll`;
-    const unenrollPath = pathWithoutParams === `/${startYear}/users/${userId}/activities/${activityId}/edit`;
-
-    if (unenrollPath) {
-      setFlyoutTitle(t('Exclude user from activity'));
-      setShowUnenrollFlyout(true);
-      setShowUserFlyout(false);
-      setShowAddEditFlyout(false);
-      setShowAddActivityFlyout(false);
-      setShowEnrollOnSchoolYear(false);
-    } else if (isEditUserPath) {
-      setFlyoutTitle(t('Edit user'));
-      setShowAddEditFlyout(true);
-      setShowUserFlyout(false);
-      setShowAddActivityFlyout(false);
-      setShowEnrollOnSchoolYear(false);
-      setShowUnenrollFlyout(false);
-    } else if (isUserActivitiesNewPath) {
-      setFlyoutTitle(t('Enroll user on activity'));
-      setShowAddActivityFlyout(true);
-      setShowUserFlyout(false);
-      setShowAddEditFlyout(false);
-      setShowEnrollOnSchoolYear(false);
-      setShowUnenrollFlyout(false);
-    } else if (isNewUserPath) {
-      setFlyoutTitle(t('Add new user'));
-      setShowAddEditFlyout(true);
-      setShowUserFlyout(false);
-      setShowAddActivityFlyout(false);
-      setShowEnrollOnSchoolYear(false);
-      setShowUnenrollFlyout(false);
-    } else if (enrollUserOnSchoolYear) {
-      setFlyoutTitle(t('Enroll user on the academic year'));
-      setShowEnrollOnSchoolYear(true);
-      setShowUserFlyout(false);
-      setShowAddEditFlyout(false);
-      setShowAddActivityFlyout(false);
-      setShowUnenrollFlyout(false);
-    } else if (isUserPath) {
-      setFlyoutTitle(t('User profile'));
-      setShowUserFlyout(true);
-      setShowAddEditFlyout(false);
-      setShowAddActivityFlyout(false);
-      setShowEnrollOnSchoolYear(false);
-      setShowUnenrollFlyout(false);
-    } else {
-      setShowUserFlyout(false);
-      setShowAddEditFlyout(false);
-      setShowAddActivityFlyout(false);
-      setShowEnrollOnSchoolYear(false);
-      setShowUnenrollFlyout(false);
-    }
-  }, [pathname]);
   const [searchQuery, setSearchQuery] = useState('');
   const [currentPage, setCurrentPage] = useState(1);
   const [userType, setUserType] = useState('all');
@@ -139,41 +64,7 @@ export function UserListContainer() {
         setCurrentPage={setCurrentPage}
       />
 
-      <FlyoutComponent
-        shouldShow={showAddEditFlyout}
-        flyoutTitle={flyoutTitle}
-        onHide={() => navigate(`/${startYear}/users`)}
-        RenderComponent={ManageProjectUserView}
-      />
-
-      <FlyoutComponent
-        shouldShow={showUserFlyout}
-        flyoutTitle={flyoutTitle}
-        onHide={() => navigate(`/${startYear}/users`)}
-        RenderComponent={UserView}
-        flyoutWidth={'800px'}
-      />
-
-      <FlyoutComponent
-        shouldShow={showAddActivityFlyout}
-        flyoutTitle={flyoutTitle}
-        onHide={() => navigate(`/${startYear}/users/${userId}`)}
-        RenderComponent={ManageStudentOnActivityContainer}
-      />
-
-      <FlyoutComponent
-        shouldShow={showEnrollOnSchoolYear}
-        flyoutTitle={flyoutTitle}
-        onHide={() => navigate(`/${startYear}/users/${userId}`)}
-        RenderComponent={EnrollStudentOnSchoolYear}
-      />
-
-      <FlyoutComponent
-        shouldShow={showUnenrollFlyout}
-        flyoutTitle={flyoutTitle}
-        onHide={() => navigate(`/${startYear}/users/${userId}`)}
-        RenderComponent={EditUserOnActivity}
-      />
+      <ProjectUserFlyouts />
     </DashboardContainer>
   );
 }
