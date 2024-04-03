978ccdd0f0ebe9959dd51574d9a19438b85b5b98
Nikola Occidentale <nikolaoccid@gmail.com>
2024-04-03 10:21:16 +0200

    Fix demo login translation

--- src/pages/login-page/demo-login.tsx
+++ src/pages/login-page/demo-login.tsx
@@ -1,5 +1,8 @@
 import styled from '@emotion/styled';
+import { useTranslation } from 'react-i18next';
+import { RiseLoader } from 'react-spinners';
 
+import { Spinner } from '../../components/spinner/spinner.tsx';
 import { useLogin } from './hooks/use-login.ts';
 
 const Container = styled.div<{ show: string }>`
@@ -17,20 +20,24 @@ const Container = styled.div<{ show: string }>`
 const Title = styled.h3`
   align-self: center;
 `;
+const Button = styled.button`
+  color: white;
+`;
 
 export const DemoLogin = () => {
+  const { t } = useTranslation();
   const { login, isLoading } = useLogin({
     email: import.meta.env.VITE_DEMO_EMAIL,
     password: import.meta.env.VITE_DEMO_PASSWORD,
   });
   if (isLoading) {
-    return <p>Loading...</p>;
+    return <Spinner SpinnerComponent={RiseLoader} color={'#2196f3'} />;
   }
   return (
     <Container show={import.meta.env.VITE_DEMO_MODE}>
       <Title>Demo login</Title>
-      <p>Note: Database restores every 45 minutes</p>
-      <button onClick={login}>Demo</button>
+      <p>{t('Note: Database restores every 45 minutes')}</p>
+      <Button onClick={login}>DEMO</Button>
     </Container>
   );
 };
